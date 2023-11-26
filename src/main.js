7c1d0563208b5cce7205908e1033063a4fcdd312
Patrik Gaicean <patrikgaicean07@gmail.com>
2023-11-27 01:21:38 +0200

    install prettier and format main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import fs from 'node:fs';
-import https from 'node:https';
+import fs from "node:fs";
+import https from "node:https";
 
 const options = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
 };
 
-https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-}).listen(8000, () => {
-  console.log("server started on https://localhost:8000")
-});
+https
+    .createServer(options, (req, res) => {
+        res.writeHead(200);
+        res.end("hello world\n");
+    })
+    .listen(8000, () => {
+        console.log("server started on https://localhost:8000");
+    });
